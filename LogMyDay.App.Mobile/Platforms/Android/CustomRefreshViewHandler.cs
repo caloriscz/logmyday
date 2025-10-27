42b1ee6e32014c8e531b8bb6d25711c4b8431412
Petr Karasek <caloris@caloris.cz>
2025-10-27 20:38:44 +0100

    Fix Android refresh handler overrides

---
+++ LogMyDay.App.Mobile/Platforms/Android/CustomRefreshViewHandler.cs
@@ -0,0 +1,77 @@
+#if ANDROID
+using Android.Content;
+using AndroidX.SwipeRefreshLayout.Widget;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace LogMyDay.App.Mobile.Platforms.Android;
+
+internal class CustomRefreshViewHandler : RefreshViewHandler
+{
+    protected override MauiSwipeRefreshLayout CreatePlatformView()
+    {
+        return new GuardedSwipeRefreshLayout(Context, this);
+    }
+
+    private sealed class GuardedSwipeRefreshLayout : MauiSwipeRefreshLayout
+    {
+        private readonly CustomRefreshViewHandler _handler;
+
+        public GuardedSwipeRefreshLayout(Context context, CustomRefreshViewHandler handler)
+            : base(context)
+        {
+            _handler = handler;
+        }
+
+        private double ScrollTolerance => (_handler.VirtualView as Controls.CustomRefreshView)?.ScrollTolerance ?? 4d;
+
+        public override bool CanChildScrollUp()
+        {
+            for (int i = 0; i < ChildCount; i++)
+            {
+                if (CanViewScrollUp(GetChildAt(i)))
+                {
+                    return true;
+                }
+            }
+
+            // No child can scroll further up -> allow pull-to-refresh gesture
+            return false;
+        }
+
+        private bool CanViewScrollUp(Android.Views.View? view)
+        {
+            if (view is null)
+            {
+                return false;
+            }
+
+            if (view.CanScrollVertically(-1))
+            {
+                return true;
+            }
+
+            if (view is Android.Webkit.WebView webView)
+            {
+                // ScrollY is in raw pixels; compare with tolerance (converted to pixels)
+                var density = Context?.Resources?.DisplayMetrics?.Density ?? 1f;
+                var tolerancePx = (int)(ScrollTolerance * density);
+                return webView.ScrollY > tolerancePx;
+            }
+
+            if (view is Android.Views.ViewGroup group)
+            {
+                for (int i = 0; i < group.ChildCount; i++)
+                {
+                    if (CanViewScrollUp(group.GetChildAt(i)))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
+#endif
