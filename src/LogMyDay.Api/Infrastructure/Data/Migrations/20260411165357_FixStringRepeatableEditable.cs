fdf9ee25674098d108ddd320f53a582f54255be8
caloriscz <caloris@caloris.cz>
2026-04-11 19:31:42 +0200

    fix: enable String repeatable, fix error-ui visibility, add tag validation

---
+++ src/LogMyDay.Api/Infrastructure/Data/Migrations/20260411165357_FixStringRepeatableEditable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogMyDay.Api.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStringRepeatableEditable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LogMyDay_InputTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsRepeatableEditable",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LogMyDay_InputTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsRepeatableEditable",
+                value: false);
+        }
+    }
+}
